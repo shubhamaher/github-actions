deac3691faf1bc14c9f27eb70c7c741e10b07bed
Ubuntu <ubuntu@ip-172-31-30-211.eu-west-2.compute.internal>
2025-05-04 21:13:46 +0000

    Final Changes

--- main.tf
+++ main.tf
@@ -11,11 +11,21 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+# Data source to find instances with the same Name tag
+data "aws_instances" "named" {
+  filter {
+    name   = "tag:Name"
+    values = ["GitHub-Actions-EC2"]
+  }
+}
+
+# Data source to get all EC2 instances
+data "aws_instances" "all" {}
+
 resource "aws_instance" "web_server" {
   ami           = "ami-0a94c8e4ca2674d5a" # Ubuntu 22.04 LTS
   instance_type = "t2.micro"
-  key_name = "devops_key"
-  
+  key_name      = "devops_key"
   tags = {
     Name = "GitHub-Actions-EC2"
   }
@@ -23,4 +33,12 @@ resource "aws_instance" "web_server" {
 
 output "public_ip" {
   value = aws_instance.web_server.public_ip
+}
+
+output "existing_named_instance_ids" {
+  value = data.aws_instances.named.ids
+}
+
+output "all_instance_ids" {
+  value = data.aws_instances.all.ids
 }
